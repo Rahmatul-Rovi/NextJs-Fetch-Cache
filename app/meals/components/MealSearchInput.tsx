5bbbe4f856312a2c0271912fe68d1a47950aecd3
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-02-17 22:35:43 +0600

    MealSearch Section Updated

--- app/meals/components/MealSearchInput.tsx
+++ app/meals/components/MealSearchInput.tsx
@@ -1,11 +1,19 @@
 "use client";
+import { usePathname, useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react'
 
 export default function MealSearchInput() {
-        const [meals, setMeals] = useState([]);
+       // const [meals, setMeals] = useState([]);
     const [search, setSearch] = useState('');
+    const router = useRouter();
+    const pathname = usePathname();
+
      useEffect(() => {
-        fetchMeals();
+        const searchQuery = {search};
+        const urlQueryParam = new URLSearchParams(searchQuery);
+        const url = `${pathname}?${urlQueryParam}`;
+        router.push(url);
+
     }, [search]); 
   return (
     <div>
