1da1f753d9045d24ef743dc7f20b472798e2e52f
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-02-28 11:01:05 +0600

    Meal page updated

--- app/Meal/page.tsx
+++ app/Meal/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect, useState } from 'react'
 
 export default function MealsPage() {
@@ -7,6 +8,7 @@ export default function MealsPage() {
         try{
         const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`);
        const data = await res.json();
+       setMeals(data?.meals || []);
        return data.meals;
         }
         catch (error){
@@ -21,8 +23,16 @@ export default function MealsPage() {
 
     }, [search]);
   return (
-    <div>
-      <p>{JSON.stringify(meals)}</p>
+    <div className='grid grid-cols-4 gap-4'>
+        {meals?.map((singleMeal)=> {
+          return(
+              <div>
+              <p className='text-2xl font-bold'>{singleMeal?.strMeal}</p>
+              <p>{singleMeal?.strInstructions}</p>
+            </div>
+        })
+          )  }
+   
     </div>
   )
 }
