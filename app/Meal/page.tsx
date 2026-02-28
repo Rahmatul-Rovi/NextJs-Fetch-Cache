31d84b39cc2ee406c3354b3ff3334793d956db72
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-02-28 10:48:45 +0600

    Meal Section Updated

--- app/Meal/page.tsx
+++ app/Meal/page.tsx
@@ -1,16 +1,28 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function MealsPage() {
     const [meals, setMeals] = useState([]);
     const [search, setSearch] = useState('');
     const fetchMeals = async () => {
-       const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`);
+        try{
+        const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`);
        const data = await res.json();
        return data.meals;
-    }
+        }
+        catch (error){
+     console.log(error);
+     return [];
+        }
+      
+    };
+
+    useEffect(() => {
+        fetchMeals();
+
+    }, [search]);
   return (
     <div>
-      
+      <p>{JSON.stringify(meals)}</p>
     </div>
   )
 }
