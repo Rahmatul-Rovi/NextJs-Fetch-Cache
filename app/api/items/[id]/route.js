5159444eef97d9fe64dd83c576ef4337dc1d463b
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-03-05 00:21:17 +0600

    Route Section Updated

--- app/api/items/[id]/route.js
+++ app/api/items/[id]/route.js
@@ -1,16 +1,16 @@
 import { ObjectId } from "mongodb";
 import dbConnect from "../../lib/dbConnect";
-export async function GET(req, params){
+export async function GET(req, {params}){
      
    const p = await params;
    const singleData = await dbConnect("users").findOne({_id: new ObjectId(p.id)})
 
-    return Response.Json({params:p});
+    return Response.Json(singleData);
 }
 
 
 
-export async function DELETE(req, params){
+export async function DELETE(req, {params}){
      
    const p = await params;
    console.log(p);
@@ -20,7 +20,7 @@ export async function DELETE(req, params){
 
 
 
-export async function PATCH(req, params){
+export async function PATCH(req, {params}){
      
    const p = await params;
    console.log(p);
