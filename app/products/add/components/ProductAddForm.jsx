8cd6ccf9d00d83b302712c535ea3c8a01b895399
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-03-06 10:46:39 +0600

    Product add section updated

--- app/products/add/components/ProductAddForm.jsx
+++ app/products/add/components/ProductAddForm.jsx
@@ -1,8 +1,20 @@
 "use client";
 
 export default function ProductAddForm() {
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
+        const from = e.target;
+        const productName = e.productName.value;
+        const payload = {productName};
+        const res = await fetch("http://localhost:3000/api/items", {
+          method: POST,
+          body: JSON.stringify(payload),
+          headers: {
+            "content-type": "application/json"
+          }
+        });
+        const result = await res.json();
+        console.log(result);
     }
   return (
     <div>
