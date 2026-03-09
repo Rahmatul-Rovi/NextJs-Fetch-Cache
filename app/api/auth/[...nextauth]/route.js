71a20c6eb1ca769f3947efcb120a5a1d4f2973a2
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-03-10 05:20:59 +0600

    AuthOption section updated

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,56 +1,8 @@
+import { authOptions } from "@/app/lib/AuthOptions"
 import NextAuth from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials";
 
-export const authOptions = {
-providers: [
-  CredentialsProvider({
-    // The name to display on the sign in form (e.g. "Sign in with...")
-    name: "Credentials",
-    // `credentials` is used to generate a form on the sign in page.
-    // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-    // e.g. domain, username, password, 2FA token, etc.
-    // You can pass any HTML attribute to the <input> tag through the object.
-    credentials: {
-      username: { label: "Username", type: "text", placeholder: "Enter your Name" },
-      password: { label: "Password", type: "password", placeholder: "Enter your Password" },
-      email: { label: "Email", type: "email", placeholder: "Enter Your Email" },
-    },
-    async authorize(credentials, req) {
-      // Add logic here to look up the user from the credentials supplied
-      const {username, password} = credentials
-      const user = await dbConnect("nextjs_fetch").findOne({username})
-      const isPasswordOk = password == user.password
-    //  const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
 
-      if (isPasswordOk) {
-        // Any object returned will be saved in `user` property of the JWT
-        return user
-      } else {
-        // If you return null then an error will be displayed advising the user to check their details.
-        return null
 
-        // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-      }
-    }
-  })
-]
-callbacks: {
-  async session ({session, token, user}){
-    if(token){
-      session.user.username = token.username;
-      session.user.role = token.role;
-    }
-    return session
-  },
-  async jwt({token, user, account, profile, isNewUser}){
-    if(user){
-      token.username = user.username;
-      token.role = user.role;
-    }
-    return token
-  }
-}
-}
 const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
