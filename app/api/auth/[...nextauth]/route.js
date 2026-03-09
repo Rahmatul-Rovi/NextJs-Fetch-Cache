cd6ee63164dc02d9a14278ea21c4b6b9f5f16e2a
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-03-09 14:40:58 +0600

    register Section updated

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -17,9 +17,12 @@ providers: [
     },
     async authorize(credentials, req) {
       // Add logic here to look up the user from the credentials supplied
-      const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+      const {username, password} = credentials
+      const user = await dbConnect("nextjs_fetch").findOne({username})
+      const isPasswordOk = password == user.password
+    //  const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
 
-      if (user) {
+      if (isPasswordOk) {
         // Any object returned will be saved in `user` property of the JWT
         return user
       } else {
