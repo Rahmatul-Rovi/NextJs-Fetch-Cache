706448b897e64f06d93027d0d1b7538cd3590d65
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-03-09 12:02:41 +0600

    Login Page Section Updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import { getServerSession } from "next-auth";
 import Image from "next/image";
+import {authOptions} from "./api/auth/[...nextauth]/route";
 
 export default function Home() {
+  const session = await getServerSession(authOptions)
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -59,6 +62,7 @@ export default function Home() {
             Documentation
           </a>
         </div>
+        <p className="font-bold text-xl">From Client Component</p>
       </main>
     </div>
   );
