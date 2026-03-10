f48da309e1c939c8dca2330b0e7ce761b59c9199
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-03-11 05:40:35 +0600

    middleware section updated

--- app/middleware.js
+++ app/middleware.js
@@ -7,7 +7,12 @@ export const middleware = async(req) => {
     const token = getToken({ req });
     const isTokenOk = Boolean(token);
     cpnst isAdminUser = token?.user == 'admin';
-    const isAdminSpecificRoute = req.nextUrl.pathname.startswith('/dashboard')
+    const isAdminSpecificRoute = req.nextUrl.pathname.startsWith('/dashboard')
+    if(isAdminSpecificRoute && !isAdminUser){
+        const callbackUrl = encodeURIComponent(req.nexturl.pathname);
+
+        return NextResponse.redirect(new URL(`/api/auth/signin?callbackUrl=${callbackUrl}`, request.url));
+    }
     if(token) console.log( "From MiddleWare:" ,token);
 
     return NextResponse.next();
