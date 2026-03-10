94d5dee9b97867bc7945fb5d03457fb44a319b52
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-03-11 00:21:18 +0600

    Middleware Section Updated

--- app/middleware.js
+++ app/middleware.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server"
 
-export default function middleware() {
-  return (
-    <div>
-      
-    </div>
-  )
+
+export const middleware = async(req) => {
+  
+    const token = getToken({ req });
+    if(token) console.log(token);
+
+    return NextResponse.next();
 }
