8591b971275fb0ddc84afa0ea5651871ab6fb1b8
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-03-11 05:26:54 +0600

    middleware section updated

--- app/middleware.js
+++ app/middleware.js
@@ -5,7 +5,10 @@ import { NextResponse } from "next/server"
 export const middleware = async(req) => {
   
     const token = getToken({ req });
-    if(token) console.log(token);
+    const isTokenOk = Boolean(token);
+    cpnst isAdminUser = token?.user == 'admin';
+    const isAdminSpecificRoute = req.nextUrl.pathname.startswith('/dashboard')
+    if(token) console.log( "From MiddleWare:" ,token);
 
     return NextResponse.next();
 }
